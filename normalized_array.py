063f68b1a1c2cdbf2a87203b00e64c20cbbbb0de
ravit-maman <ravittu@ac.sce.ac.il>
2026-04-15 15:45:46 +0300

    Update normalized_array.py

--- normalized_array.py
+++ normalized_array.py
@@ -14,7 +14,6 @@ def normalize_array(input_array):
     normalized = (arr - min_val) / (max_val - min_val)
 
     return normalized
-
 if __name__ == "__main__":
     # כאן הסטודנטים יכולים להריץ בדיקה עצמית מהירה
     test_data = [10, 20, 30, 40, 50]
