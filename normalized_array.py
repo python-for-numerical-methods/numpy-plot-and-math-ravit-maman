5eb58e441f071febf5c71aef3403f842c1d19904
ravit-maman <ravittu@ac.sce.ac.il>
2026-04-15 15:58:25 +0300

    Update normalized_array.py

--- normalized_array.py
+++ normalized_array.py
@@ -17,6 +17,6 @@ def normalize_array(input_array):
     
 if __name__ == "__main__":
     # כאן הסטודנטים יכולים להריץ בדיקה עצמית מהירה
-    test_data = [10, 20, 30, 40, 50]
-    print(f"Original: {test_data}")
+    test_normalized = [10, 20, 30, 40, 50]
+    print(f"Original: {test_normalized}")
     print(f"Normalized: {normalized_array(test_data)}")
