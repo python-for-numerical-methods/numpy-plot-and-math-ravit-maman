1d07977c5f7d4417c9d00ccd3f91e9f45bc34c18
ravit-maman <ravittu@ac.sce.ac.il>
2026-04-15 15:50:18 +0300

    Update normalized_array.py

--- normalized_array.py
+++ normalized_array.py
@@ -1,14 +1,16 @@
 import numpy as np
 
 def normalize_array(input_array):
-    arr = np.asarray(input_array)
-   
+    arr = np.asarray(input_array)  # Ensure it's a NumPy array
+
     min_val = np.min(arr)
     max_val = np.max(arr)
 
+    # Handle the case where all values are equal
     if max_val == min_val:
         return np.zeros_like(arr, dtype=float)
 
+    # Normalize
     normalized = (arr - min_val) / (max_val - min_val)
 
     return normalized
